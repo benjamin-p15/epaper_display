e3a632f27973c2da37a7d926999669e2756b6f0b
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-12-27 18:52:18 -0700

    test

--- dashboard/modules/weather/main.py
+++ dashboard/modules/weather/main.py
@@ -56,7 +56,7 @@ def render():
             for key, value in metar_dict.items():
                 print(f"{key}: {value}")
 
-        for i in range(7): screen.add_rectangle(position=(0+i*0.145, 0.75), size=(0.14,0.2), fill=0, radius=15, thickness=2)
+        for i in range(7): screen.add_rectangle(position=(0+i*0.145, 0.75), size=(0.135,0.2), fill=0, radius=15, thickness=2)
         _cache_img=screen.render()
 
         if(_cache_img is None): return None, False
