45c2da94358f9e77d933687ace363c270ae450d9
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-01-21 16:27:13 -0700

    test

--- external/start_epaper_display.sh
+++ external/start_epaper_display.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e
 
-REPO_DIR="/home/benjaminpi2/epaper_display/external/start_epaper_display.sh"
+REPO_DIR="/home/benjaminpi2/epaper_display"
 SCRIPT="external/main.python3"
 
 echo "Waiting for internet..."
-
 until ping -c1 -W1 8.8.8.8 >/dev/null 2>&1; do
   sleep 2
 done
-
 echo "Internet is up"
 
 cd "$REPO_DIR"
-
-echo "Pulling latest code..."
-git pull --rebase
+echo "Updating repository..."
+git fetch origin
+git reset --hard origin/master
 
 echo "Starting epaper script..."
 exec /usr/bin/python3 "$REPO_DIR/$SCRIPT"
