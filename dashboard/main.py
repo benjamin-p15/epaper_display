ddc6ad91d979eabcdce366d7171b9aff82b0ec40
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-03-14 01:20:25 -0600

    test

--- dashboard/main.py
+++ dashboard/main.py
@@ -128,10 +128,9 @@ def display_loop(display):
 
 # Handle button interface switcher
 def button_loop():
-    print("loop started")
     global current_layout, update_state
     while True:
-        print("loop cycle")
+        print(f"{GPIO.input(BUTTON_PIN)}")
 
         if GPIO.input(BUTTON_PIN) == GPIO.LOW:
             print(" PUSH ")
