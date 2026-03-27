959ed2fc5af059fc2f03ed5d30ffb571571c2448
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-03-27 09:25:49 -0600

    test

--- dashboard/modules/canvas/main.py
+++ dashboard/modules/canvas/main.py
@@ -96,9 +96,11 @@ def render(force=False):
             due_box_w = 0.084
 
             # Draw three boxes, one around course name, assigment name, and due date 
-            screen.add_rectangle(position=(course_box_x, y - 0.02), size=(course_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
-            screen.add_rectangle(position=(assignment_box_x, y - 0.02), size=(assignment_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
-            screen.add_rectangle(position=(due_box_x, y - 0.02), size=(due_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
+            #screen.add_rectangle(position=(course_box_x, y - 0.02), size=(course_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
+            #screen.add_rectangle(position=(assignment_box_x, y - 0.02), size=(assignment_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
+            #screen.add_rectangle(position=(due_box_x, y - 0.02), size=(due_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
+
+            screen.add_rectangle(position=(course_box_x, y - 0.02), size=(course_box_w+assignment_box_w+due_box_w, y_gap - 0.01), fill=None, radius=4, thickness=5)
 
             # Add course name, and due date to screen
             screen.add_text([{"text": assignment['course_name'], "size": font_size}], position=(course_box_x + 0.01, y), align="left")
