221135642bece81fd614d7bce0a0f1666776a1dd
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-05-28 10:24:07 -0600

    changes

--- dashboard/main.py
+++ dashboard/main.py
@@ -37,7 +37,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 update_state = False
 image_threshold = 128
-layouts=["stocks", "planetary_clock", "clock", "canvas", "weather", "analog_clock"] #"image", "grapher"
+layouts=["stocks", "clock", "weather", "analog_clock"] #"stocks", "planetary_clock", "clock", "canvas", "weather", "analog_clock"
 current_layout = layouts[1]
 ENABLE_WEB = False
 force_render = False
